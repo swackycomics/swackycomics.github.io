86a2c856626fad95d74ee633a33011867592df08
swackycomics <qwertyguy3361@gmail.com>
2024-11-12 03:43:37 +0900

    header revamp

--- js/generic.js
+++ js/generic.js
@@ -5,25 +5,31 @@ let comicarray = [
     ["vegg_n_nameless_comics","Dr. Vegg \'n Nameless Comics (Discontinued)"]
 ];
 
+document.write("<div style=\"background-color: #000000\" id=\"mousediv\">\r\n  <p id=\"hovertext\"><\/p>\r\n<\/div>");
+
+document.addEventListener('mousemove', function(ev){
+	
+    document.getElementById('mousediv').style.transform = 'translateY('+(ev.clientY+12)+'px)';
+    document.getElementById('mousediv').style.transform += 'translateX('+(ev.clientX-32)+'px)';            
+},false);
+
+function showtext(type){
+	var texts = ["News","Comics","Trivia","Funny"];
+	if(type == -1){
+		document.getElementById("hovertext").textContent="";
+	} else {
+		document.getElementById("hovertext").textContent=texts[type];
+	}
+}
+
 document.write( '<div class=\"left-border align-left on-top\" style=\"width:186px; height:100%;\"></div>\n' );
 document.write( '        <div class=\"right-border align-right on-top\" style=\"width:186px; height:100%;\"></div>\n' );
 document.write( '        <div class=\"align-center header on-top\" style=\"width:100%;\">\n' );
-document.write( '            <a class="header-button" id=\"header1\"></a>\n' );
-document.write( '            <a class="header-button" id=\"header2\" href=\"/\"></a>\n' );
-document.write( '<a class="header-button" id=\"header3\" onclick=\"dropdown()\" class=\"dropbtn\"></a>\n' );
-document.write( '            <div id=\"comicdropdown\" style="display: none;" class=\"dropdown-content\">\n' );
-for(var i = 0; i < comicarray.length; i++){
-    document.write( '                <a class=\"dropdown-button\" href=\"' +'/comics/'+comicarray[i][0]+'/'+ '\">' +comicarray[i][1]+ '</a>\n' );
-}
-document.write( '            </div>' );
-document.write( '            <a class="header-button" href=\"/trivia/\" id=\"header5\"></a>\n' );
-document.write( '            <a class="header-button" href=\"/funny/\" id=\"header4\"></a>\n' );
 
-document.write( '<script>\n' );
-document.write( '                //comic dropdown menu\n' );
-document.write( '                function dropdown() {\n' );
-document.write('                     document.getElementById(\"comicdropdown\").style.display = document.getElementById(\"comicdropdown\").style.display === \'none\' ? \'\' : \'none\';\n');
-document.write( '                }\n' );
-document.write( '            </script>' );
+document.write( '            <a class="header-button" href=\"/\" id=\"header1\"></a>\n' );
+document.write( '            <a class="not-logo header-button" href=\"/news/\" onmouseout="showtext(-1)" onmouseover="showtext(0)" id=\"header2\"></a>\n' );
+document.write( '            <a class="not-logo header-button" href=\"/comics/\" onmouseout="showtext(-1)" onmouseover="showtext(1)" id=\"header3\"></a>\n' );
+document.write( '            <a class="not-logo header-button" href=\"/trivia/\" onmouseout="showtext(-1)" onmouseover="showtext(2)" id=\"header4\"></a>\n' );
+document.write( '            <a class="not-logo header-button" href=\"/funny/\" onmouseout="showtext(-1)" onmouseover="showtext(3)" id=\"header5\"></a>\n' );
 
 document.write( '        </div>' );
---
+++ js/loadrecent.js
@@ -0,0 +1,8 @@
+//recent comic
+document.write( '<div class=\"recent\">\n' );
+document.write( '                <table style=\"width:90%; min-width:500px;\">\n' );
+document.write( '                    <tr>\n' );
+document.write( '                <td><p id=\"ptop\">MOST RECENT COMIC!</p><div><a href=\"'+recent.href+'\"><img src=\"/img/icons/'+recent.thumb+'.png\" width=\"71.3%\" height=\"71.3%\"><div id=\"new\"></div></div><p id=\"pbottom2\">'+recent.title+'</p></td>\n' );
+document.write( '                    </tr>\n' );
+document.write( '                </table>\n' );
+document.write( '            </div>' );
