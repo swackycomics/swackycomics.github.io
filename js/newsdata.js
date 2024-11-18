4b391c21d2cb7c3176e1295dc9c6441865a38064
swackycomics <qwertyguy3361@gmail.com>
2024-11-19 03:27:44 +0900

    swackzards news

--- js/newsdata.js
+++ js/newsdata.js
@@ -1,5 +1,27 @@
 var newsdata = [
 	{
+		title: "Brand new comic series & changes to the site",
+		date: "November 18, 2024",
+		fields: [
+			[
+				"text",
+				[
+					'<p>Why, hello and welcome to the second news post on the news posting section of swackycomics where the news are, in fact, getting posted. It is I, swackyGCF. And I have come here to announce a BRAND NEW (!!!) comic series. That&#39;s right. It&#39;s SWACKZARDS! Starring none other than two wizards named Styx and Crius. The first comic is available <a href="/comics/swackzards_comics/ep1/" target="_blank">right here</a> if you wanna check it out!</p>',
+					'<p>For this one, I wanted to give it a new &quot;style&quot;, so to speak. You can notice some of the things look a bit different from my usual comics. The lineart, the backgrounds, etc etc.</p>',
+					'<p>I&#39;ve also tried to give the characters new expressions in each panel, which was partially inspired by Ren &amp; Stimpy&#39;s &quot;never the same face twice&quot; rule, though not to the same extent, obviously.</p>'
+				]
+			],
+			["image","/news/img/news2.png"],
+			[
+				"text",
+				[
+					'<p>In other news, I&#39;ve implemented a <a href="/contact/" target="_blank">CONTACT</a> section. I believe some may find it useful!!</p>',
+					'<p>That&#39;s about it for this post. See you when I make a new one.</p>'
+				]
+			]
+		]
+	},
+	{
 		title: "Swacknews. Moving to Github Pages & More",
 		date: "November 11, 2024",
 		fields: [
