dffe8d9911ad31e0e0cc6176f4b5246a21c56c27
swackycomics <qwertyguy3361@gmail.com>
2024-11-24 03:57:23 +0900

    jlfj 2 page

--- js/screenshots.js
+++ js/screenshots.js
@@ -1,5 +1,5 @@
 //define "dir" before referring to this script | example dir = "/img/games/dynamitehead/"
-var len = 7;
+//define "len" before referring to this script | example len = 7
 
 for(var i = 1; i < len+1; i++){
 	document.write('<a href="'+dir+"scr"+(i.toString())+'.png" target="_blank">');
