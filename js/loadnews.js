34ae859993a67fb80470d6dbdc51a19014a441c9
swackycomics <qwertyguy3361@gmail.com>
2024-11-27 07:34:30 +0900

    news image preview

--- js/loadnews.js
+++ js/loadnews.js
@@ -1,10 +1,18 @@
 function loadNews(type){
 	if(type == "all"){
 		for(var i = 0; i < newsdata.length; i++){
+			var sentimage = false;
 			var curnews = newsdata[i];
 			document.write('<div id="news-full" class="news-container yellow-border align-center" style="cursor: pointer;" onclick="window.location=\''+curnews.href+'\'">');
 			document.write('<p class="big-text">' + curnews.title + '</p>');
 			document.write('<p class="small-text">' + curnews.date + '</p>');
+			for(var j = 0; j < curnews.fields.length; j++){
+				var curfield = curnews.fields[j];
+				if(!sentimage && curfield[0] == "image"){
+					document.write('<img src="'+curfield[1]+'">');
+					sentimage = true;
+				}
+			}
 			document.write('</div>');
 		}
 	} else if(type == "one"){
