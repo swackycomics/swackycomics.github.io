c86daac064cd3559f4428da10db1be27b3e5fc41
swacky gcf <qwertysxdg3361@gmail.com>
2025-03-18 00:06:32 +0900

    fanart corner

---
+++ js/fanartdata.js
@@ -0,0 +1,194 @@
+var artdata = [
+	{
+		img: "bafkreif2ylrufan33cnzo2jwvzwmylczcdhgoguxqd6r2n2aghlq64socy.jpg",
+		date: "Mar. 15, 2025",
+		desc: "popkinsssussy (bluesky)",
+		category: 4
+	},
+	{
+		img: "bafkreidtuujrqxvnwk5olhlk6jbv2fbpbxyutqnxdl3p7z2suc6kn4whny.jpg",
+		date: "Mar. 6, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_swackzards1.jpg",
+		date: "Feb. 28, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_swackzards2.jpg",
+		date: "Feb. 28, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_swackzards3.jpg",
+		date: "Feb. 28, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_swackzards4.jpg",
+		date: "Feb. 28, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_rb1.jpg",
+		date: "Feb. 26, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_rb2.jpg",
+		date: "Feb. 26, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_rb3.jpg",
+		date: "Feb. 26, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_rb4.jpg",
+		date: "Feb. 26, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "bafkreibwbopjqhnfdcjwguqalkdg6l2wxjv7cjyejtqf7kr62ftb2haiaq.jpg",
+		date: "Feb. 26, 2025",
+		desc: "frolichlazy (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_dhfeb_4.jpg",
+		date: "Feb. 3, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_dhfeb_3.jpg",
+		date: "Feb. 3, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_dhfeb_2.jpg",
+		date: "Feb. 3, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "toki_dhfeb_1.jpg",
+		date: "Feb. 3, 2025",
+		desc: "toki-ch (bluesky)",
+		category: 4
+	},
+	{
+		img: "bafkreiej3rydxj5npw73k2ncr574lrxnwnlbgus2chrote23f2waznzwza.jpg",
+		date: "Jan 22, 2025",
+		desc: "murilorayman (bluesky)",
+		category: 4
+	},
+	{
+		img: "bricklib_dh.png",
+		date: "Dec. 24, 2024",
+		desc: "BRICKLIB",
+		category: 3
+	},
+	{
+		img: "ng_dh2.png",
+		date: "Nov 4, 2024",
+		desc: "Plantgamer09",
+		category: 3
+	},
+	{
+		img: "dh2024ufohare.png",
+		date: "Sep 15, 2024",
+		desc: "UFO-HARE",
+		category: 3
+	},
+	{
+		img: "ng_dh1.png",
+		date: "Sep 14, 2024",
+		desc: "Vantas69",
+		category: 3
+	},
+	{
+		img: "swack2024.png",
+		date: "Jul. 15, 2024",
+		desc: "charlesalien",
+		category: 3
+	},
+	{
+		img: "IMG_3900.png",
+		date: "Feb. 7, 2024",
+		desc: "pvt sherman",
+		category: 3
+	},
+	{
+		img: "GFWoh9OXMAAdObl.png",
+		date: "Feb. 3, 2024",
+		desc: "charlesalien",
+		category: 3
+	},
+	{
+		img: "Untitled114_20240125232740.png",
+		date: "Jan 26, 2024",
+		desc: "sekritari",
+		category: 3
+	},
+	{
+		img: "unknown2.png",
+		date: "Mar. 9, 2022",
+		desc: "SwellOcean",
+		category: 1
+	},
+	{
+		img: "No_Pain_No_Gain.png",
+		date: "Feb. 6, 2022",
+		desc: "LanRicks",
+		category: 1
+	},
+	{
+		img: "redbidd.png",
+		date: "Jan. 29, 2022",
+		desc: "SwellOcean",
+		category: 1
+	},
+	{
+		img: "tipbox.png",
+		date: "Jan. 29, 2022",
+		desc: "SwellOcean",
+		category: 1
+	},
+	{
+		img: "Untitled2093_20211224101155.png",
+		date: "Dec. 25, 2021",
+		desc: "mintyfresh2490",
+		category: 0
+	},
+	{
+		img: "unknown.png",
+		date: "Dec. 23, 2021",
+		desc: "SwellOcean",
+		category: 0
+	},
+	{
+		img: "BEAT_UP.png",
+		date: "Dec. 23, 2021",
+		desc: "LanRicks",
+		category: 0
+	},
+	{
+		img: "20211222_125213.png",
+		date: "Dec. 22, 2021",
+		desc: "navyneurons (kincd)",
+		category: 0
+	},
+];	
