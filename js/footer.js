6908dfb442b491f87493672676442e1d73952618
swacky gcf <qwertysxdg3361@gmail.com>
2025-12-28 12:41:32 +0900

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,2 +1,2 @@
-var curdate = "Dec. 26, 2025";
+var curdate = "Dec. 27, 2025";
 document.write("<div class=\"bottom\" style=\"width: 100%; text-align:center; height: 64px\">\r\n    <p>SWACKYCOMICS - WHEN THERE'S SWACK, THERE'S COMIC<\/p><p>Last update: "+curdate+"<\/p><br\/>\r\n<\/div>");
