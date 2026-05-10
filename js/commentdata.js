50a645a074cc9da98d1ca9a1a5e828df117ef400
swacky gcf <qwertysxdg3361@gmail.com>
2026-05-10 17:52:42 +0900

    Update commentdata.js

--- js/commentdata.js
+++ js/commentdata.js
@@ -1,4 +1,5 @@
 var comment_data = [
+	{ page: "/news/article/comment_section/", user: "tapok!!", date: "May 10, 2026 at 07:06 AM", comment: "I freackin love it✌️", },
 	{ page: "/news/article/comment_section/", user: "Hate Commenter", date: "May 9, 2026 at 05:12 PM", comment: "I hate it.", },
 	{ page: "/news/article/comment_section/", user: "Mira", date: "May 9, 2026 at 12:52 PM", comment: "it's... peak", },
 	{ page: "/news/article/comment_section/", user: "sxtw_", date: "May 9, 2026 at 12:27 PM", comment: "peak", },
