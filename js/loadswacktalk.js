4b36b39aae74a1fd185189b8708c307ca1a3ee82
swacky gcf <qwertysxdg3361@gmail.com>
2026-05-14 03:19:30 +0900

    toxic demo swacktalk post

--- js/loadswacktalk.js
+++ js/loadswacktalk.js
@@ -1,38 +1,56 @@
 function loadSwacktalk(type){
+	var has_player_tags = false;
+	
 	if(type == "all"){
 		for(var i = 0; i < swacktalk.length; i++){
-			var sentimage = false;
 			var curblog = swacktalk[i];
 			document.write('<div id="news-full" class="news-container yellow-border align-center" style="cursor: pointer;" onclick="window.location=\''+curblog.href+'\'">');
 			document.write('<p class="big-text">' + curblog.title + '</p>');
 			document.write('<p class="small-text">' + curblog.date + '</p>');
 			document.write('<p>' + curblog.shortdesc + '<strong style="color:#FFB812"> Read more</strong></p>');
-			for(var j = 0; j < curblog.fields.length; j++){
-				var curfield = curblog.fields[j];
-				if(!sentimage && curfield[0] == "image"){
-					document.write('<img src="'+curblog.thumbnail+'">');
-					sentimage = true;
-				}
-			}
+			document.write('<img src="'+curblog.thumbnail+'">');
 			document.write('</div>');
 		}
 	} else if(type == "one"){
 		var id = document.querySelectorAll("#newsid")[0].innerHTML;
 		var curblog = 0;
+		var curblog_fields = 0;
 		for(var i = 0; i < swacktalk.length; i++){
 			if(swacktalk[i].id == id){
 				curblog = swacktalk[i];
 				break;
 			}
 		}
+		
+		curblog_fields = swacktalk_current[0].fields;
+		
 		document.write('<div id="news-full" class="news-container yellow-border align-center">');
 		document.write('<p class="big-text">' + curblog.title + '</p>');
 		document.write('<p class="small-text">' + curblog.date + '</p>');
-		for(var j = 0; j < curblog.fields.length; j++){
-			var curfield = curblog.fields[j];
+		for(var j = 0; j < curblog_fields.length; j++){
+			var curfield = curblog_fields[j];
 			//write fields
 			if(curfield[0] == "image"){
 				document.write('<img src="'+curfield[1]+'">');
+				if(curfield.length >= 3){
+					document.write('<p class="align-center" id="underimg_text">'+curfield[2]+'</p>');
+				}
+			}
+			if(curfield[0] == "video"){
+				has_player_tags = true;
+				document.write('<video preload="metadata" width="720" class="mejs__player" data-mejsoptions=\'{"alwaysShowControls": "true"}\'">');
+				document.write('	<source src="'+curfield[1]+'" type="video/mp4"\'>');
+				document.write('	WHOOPS! Your browser doesn\'t support video files!');
+				document.write('</video>');
+				if(curfield.length >= 3){
+					document.write('<p class="align-center" id="underimg_text">'+curfield[2]+'</p>');
+				}
+			}
+			if(curfield[0] == "audio"){
+				has_player_tags = true;
+				document.write('<audio src="'+curfield[1]+'" class="mejs__player">');
+				document.write('	WHOOPS! Your browser doesn\'t support audio files!');
+				document.write('</audio>');
 				if(curfield.length >= 3){
 					document.write('<p class="align-center" id="underimg_text">'+curfield[2]+'</p>');
 				}
@@ -44,5 +62,17 @@ function loadSwacktalk(type){
 			}
 		}
 		document.write('</div>');
+	}
+	
+	if(has_player_tags){
+		// You can use either a string for the player ID (i.e., `player`), 
+		// or `document.querySelector()` for any selector
+		var player = new MediaElement('player', {
+			iconSprite: '/css/mediaelement/mejs-controls.svg', 
+			features: ['playpause', 'current', 'progress', 'duration', 'volume', 'tracks', 'fullscreen'],
+			success: function(mediaElement, originalNode, instance) {
+			
+			}
+		});
 	}
 }
---
+++ js/swacktalk/swacktalk3.js
@@ -0,0 +1,371 @@
+var swacktalk_current = [
+	{
+		fields: [
+			["image","/swacktalk/img/blog3_thumb.png"],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>What up everybody? Swack here once again with a brand new Swacktalk post! Been a bit since the last one.</p>",
+					"<br/>",
+					"<p>This time I (and others, stay tuned for that) will be discussing the development of The Toxic Demo. </p>",
+					"<p>I'm gonna try to make this rather brief to cover everything from the first alpha up until the release of this demo. LET'S GET TO IT!</p>",
+					"<br/>",
+				]
+			],
+			[
+				"text",
+				[
+					"<h1>THE ALPHA DAYS</h1>",
+					"<p>So, the prototype's done. Now it's time to make this whole thing come to fruition.</p>",
+					"<br/>",
+					"<p>If I remember things correctly, the first thing I've implemented was the run toggle. You know how it works, double tap and DynamiteHead begins to leg it. Watch him go. It looked real janky back then. Genuinely, look at this. THIS SUCKS!!!!!!!!!!!</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_gif1.gif","DynamiteHead, running."],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>But who needs all that when you've got BADHEADS to beat up? That's right. Next up on the menu is ENEMY ANIMATIONS. Or rather, unfinished.. enemy animations. </p>",
+					"<p>Those might seem familiar to some of you who saw the first teaser of this game posted on my Youtube channel. Long time ago that was, long time ago.</p>"
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_gif2.gif","Here they are."],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>SO! The animations are here. Now what? Now we check 'em out in game.</p>",
+					"<br/>",
+					"<p>One thing I gotta note is that the animations had to be resized at runtime to be 60% of its imported size. As you can probably tell, this impacted the game's performance significantly on HTML. It's been like that up until The Monyx Fever update. Anyway, I'm totally getting sidetracked over here. Let's take a peek at those badheads already.</p>",
+				]
+			],
+			["video", "/swacktalk/vid/swacktalk3/blog3_vid1.mp4","It's mayhem out here..."],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>Hearing that music? JarekTEK helped out with that stuff. There's a whole bunch of unused soundtracks, actually. </p>",
+					"<p>I'm gonna take a step back from the script and let him speak for a bit. He knows the nitty-gritty of it better than I do.</p>",
+					"<br/><br/>",
+				]
+			],
+			
+			/* jarektek part */
+			
+			[
+				"text",
+				[
+					"<img class=\"swacktalk_guest\" src=\"/swacktalk/img/swacktalk_jarek.png\">",
+					"<div id=\"guest_jarektalk\">",
+				]
+			],
+			
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>Oho!? It's my turn now?! Really? Wow.</p>",
+					"<br/>",
+					"<p>Anyway, hi. It's me, Jarek. You may have HEARD that I'm the guy who composed most of the music in this game. Definitely true. Yep.</p>",
+					"<p>Well, I could talk forever about the entire process of how I've done everything, but that would take FOREVER. So, I'm just going to talk about everything that was unused in the Toxic Demo.</p>",
+					"<br/>",
+					"<p>I'm gonna start with the FIRST EVER song I made for this game, which is this.</p>",
+				]
+			],
+			["audio", "/swacktalk/audio/swacktalk3/orua.mp3","The first song...!"],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>So, this is quite heavily reflected in the prototype days, but Swack originally wanted the music to sound more like Sega Genesis music. \"something akin to streets of rage 3 but with a twist\"</p>",
+					"<br/>",
+					"<p>Of course, I was still learning how to properly make music, so it's a little iffy... and this is also not the direction for the music anymore.</p>",
+					"<p>Back then, neither of us really knew what the direction of the music was going to be almost at all.</p>",
+					"<p>... that's why some of the early music sounds like the Pizza Tower OST.</p>",
+					"<br/>",
+					"<p>Anyway, I have here the first DYNAMITEHEAD stage music. This was even before Toxic Trenches was even conceived, I believe.</p>",
+				]
+			],
+			["audio", "/swacktalk/audio/swacktalk3/DynamiteCity_forgotten.mp3","SO RETRO"],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>Not really much to say about this one... </p>",
+					"<p>You've all probably heard the prototype version of Toxic Trenches that actually did get used for a bit, since it's in the OST. (Maybe one day I'll share all of the WIPs as well...)</p>",
+					"<p>But, in case you haven't, here's the link to the OST... <a href=\"https://soundcloud.com/jarektek/sets/dynamitehead-tntson\" target=\"_blank\">Toxic Demo OST</a></p>",
+					"<br/>",
+					"<p>By the way, the version of Toxic Trenches you actually do get to hear in the released demo of the Toxic Demo was made a month after the prototype version! I wonder what changed in that time... Hmmm (Me, thinking)</p>",
+					"<br/>",
+					"<p>Here's an early version of the version that DID get used.</p>",
+				]
+			],
+			["audio", "/swacktalk/audio/swacktalk3/dh_toxictrenches_TheBest.mp3","Wow... It's like you're underwater."],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>Also, for some reason, I wanted the pause music for each stage to be a jazzy version of itself???</p>",
+				]
+			],
+			["audio", "/swacktalk/audio/swacktalk3/jazzytrencheswip2.mp3","Incredible..."],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>I also have the first attempt at a menu theme... But, do be warned, the guitar in this is kind of loud.</p>",
+				]
+			],
+			["audio", "/swacktalk/audio/swacktalk3/earlydinamit.mp3",""],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>I would share the WIPs for the menu theme that did get used, but that's for another time! I think!!!</p>",
+					"<br/>",
+					"<p>But really, making the FRANKENBARF theme was the hardest part. It took me lots of tries and attempts to finally make the one heard in the demo.</p>",
+					"<p>To the point where it was the only thing left to do... this was not the last time that happened...</p>",
+				]
+			],
+			["audio", "/swacktalk/audio/swacktalk3/frank.mp3","Sounds like a zKevin song..."],
+			["audio", "/swacktalk/audio/swacktalk3/frankenbarfwip.mp3","Second attempt"],
+			["audio", "/swacktalk/audio/swacktalk3/barf.mp3",""],
+			["audio", "/swacktalk/audio/swacktalk3/frank2.mp3",""],
+			["audio", "/swacktalk/audio/swacktalk3/AnotherFrankWip.mp3",""],
+			["audio", "/swacktalk/audio/swacktalk3/just straight up. frankin' it..mp3",""],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>For some reason, I decided to also make a Groovy Graveyard theme in the middle of the Toxic Demo development. In August of 2024. It doesn't sound like this at all anymore...</p>",
+				]
+			],
+			["audio", "/swacktalk/audio/swacktalk3/groovygraveyardwip5.mp3","So funky! This actually doesn't sound bad!"],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>Anyway, most of the time I've spent making music for this game is just experimenting what might sound good or not. Even in the Groovy Demo...</p>",
+					"<br/>",
+					"<p>There's probably a whole plethora of attempts and experiments I haven't even exported into audio files. Going through those would take a while though. And maybe I could save those for another time...</p>",
+					"<br/>",
+					"<p>I actually would love to delve deeper into sharing my old music for this game. And all the unused ones... I love that kind of stuff...</p>",
+					"<br/>",
+					"<p>So, until next time...! When I get to share everything I did for the Groovy Demo...!</p>",
+					"<p>I just have one more thing to show.</p>",
+				]
+			],
+			["audio", "/swacktalk/audio/swacktalk3/ilikehotdogs.wav",""],
+			
+			[
+				"text",
+				[
+					"<br/>",
+					"</div>", //close swacktalk guest div
+				]
+			],
+			
+			/* end of jarektek part */
+			
+			[
+				"text",
+				[
+					"<br/><br/>",
+					"<p>Absolutely great and incredible insight from Jarek. Told you he knows what's up.</p>",
+					"<br/>",
+					"<p>Here's one feature I remember having fun implementing into the game - \"THE TOXIC SHACK\". The way the lighting here works is as such: There's an invisible trigger object in the room, which changes the stage lighting upon collision with DynamiteHead.</p>",
+					"<p>Bit of a fun fact about this one... I borrowed the code for this from Jimmy's Lost Fruits Journey 2. The shack itself had a lot more enemies in it during the alpha phase. It was a tougher place back then, alright.</p>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_img1.png"],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>Not much to say about the versus screen, considering it looks about the same as it does in the final version. The concept of it is something I've also decided to borrow from JLFJ 2. I didn't even bother replacing the graphics here. WHAT A TRAVESTY!</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_img2.png"],
+			["image", "/swacktalk/img/swacktalk3/blog3_img3.png","Spot 740948394015 differences"],
+			[
+				"text",
+				[
+					"<h1>THE DAYS OF BETA</h1>",
+					"<p>Moving onto the next stage of development. I've spent most of it remaking all of the animations and graphics in the game to look \"EVEN BETTER!\".</p>",
+					"<p>All you need to know about the beta is that it straight up looks the same as the release version, except DynamiteHead's colors look different (They weren't being affected by the shader) and the proper stage graphics are missing.</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_img4.png"],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>ALSO! Here's an unused DynamiteHead animation. I guess.</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_gif3.gif","(Goin' somewhere.)"],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>Okay, from here on out I'm gonna hand the script over to Onsku.</p>",
+					"<p>Alright, bud. Tell us about your contributions to the demo. :)</p>",
+					"<br/>",
+				]
+			],
+			
+			/* onsku part */
+			
+			[
+				"text",
+				[
+					"<img class=\"swacktalk_guest\" src=\"/swacktalk/img/swacktalk_onsku.png\">",
+					"<div id=\"guest_onskutalk\">",
+				]
+			],
+			
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>WHAT'S UP SWACKYCOMICS! It's me. The Onsku And Only. I contributed a bunch of stuff to this demo all the way from design, concepts and code to the HTML version of the game existing. Now you might be wondering, HTML version? Not existing in some alternate timeline? It's a long story. STAY TUNED FOR THAT ONE...</p>",
+					"<br/>",
+					"<p>Okay. Let's start off with the CONCEPTS and FEATURES that I contributed to this game. I'm not gonna go over all of them because I genuinely can't remember every single one, but here's some of the more notable ones.</p>",
+					"<br/>",
+					"<h2>UPPERCUT!</h2>",
+					"<p>The uppercut was actually a thing I came up with. One of the first concepts I came up with in fact. All the way back from the prototype days when you had to hold the up key and press punch. JANK! As you all know, this was eventually changed in the Monyx Fever demo.</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_img5.png",""],
+			[
+				"text",
+				[
+					"<br/>",
+					"<h2>SURF INTRO!</h2>",
+					"<p>Yep. The iconic intro of each stage. I worked on making that look a lot nicer. It didn't look that great initially, so one of the things I did to improve is that I made the movement in it a lot more smooth and fluid.</p>",
+					"<br/>",
+					"</div>",
+				]
+			],
+			["video", "/swacktalk/vid/swacktalk3/blog3_vid2.mp4","WOW! Look at him go."],
+			[
+				"text",
+				[
+					"<div id=\"guest_onskutalk\">",
+					"<br/>",
+					"<h2>DAMAGE NUMBERS!</h2>",
+					"<p>Those red numbers you see popping up when you punch enemies? Yep, they're showing how much damage you're dealing. I Did That. Everybody likes the damage numbers, they show you which attack you should spam instead of doing all the other attacks. Big number, big damage. Eventually these were expanded to show healing as well, BUT THAT'S FOR THE NEXT DEMO...</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_gif4.gif"],
+			[
+				"text",
+				[
+					"<br/>",
+					"<h2>TNT-QUAKE VISUALS!</h2>",
+					"<p>Well, back then it was called the TNT-BLAST. Then some red alcoholic guy called and we changed it. That was a joke. ANYWAYS... Before this, the Quake looked a little too static for my taste so I made this concept to visualize my new vision for it. It turned out to look Quite Good if I do say so myself. Also, my Microsoft Paint art is back.</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_img6.png",""],
+			["image", "/swacktalk/img/swacktalk3/blog3_gif5.gif",""],
+			[
+				"text",
+				[
+					"<p>NOW. Let's talk about some of the INTERESTING IDEAS that didn't make the cut. There's probably a bunch more of these but I can't be bothered to find them.</p>",
+					"<br/>",
+					"<h2>BARRELS AND BEARTRAPS!</h2>",
+					"<p>Now these were not added for whatever reason. Blame Swack I guess. Just kidding. Not really. It's pretty obvious what these would've done based on the concepts below, unless you're stupid or something. You're not stupid, right? Maybe you'll see them again at a later point in development... Who Knows?</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_img7.png",""],
+			["image", "/swacktalk/img/swacktalk3/blog3_img8.png","Don't make fun of my messy art. Or I Will Find You."],
+			[
+				"text",
+				[
+					"<br/>",
+					"<h2>ARENA MODE!</h2>",
+					"<p>The Arena Mode was a neat wave-based gamemode I cooked up in an evening. It wasn't finished at all, but it was more or less a proof of concept version. Here's the rundown on how it works:</p>",
+					"<br/>",
+					"<p>The gamemode is wave-based, this means that each wave, some amount of enemies are spawned based on the wave difficulty (they get more difficult as you go on). Once these enemies have been defeated, the next wave starts. You rinse and repeat this until you reach a BOSS WAVE. During this wave, a boss gets spawned (at this point there was only frankenbarf). Once you've defeated them, you get to choose some upgrades or power ups. A true roguelike experience. </p>",
+					"<p>You may see this gamemode return in the future as well... Only time will tell.</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_img9.png",""],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>AND LAST BUT NOT LEAST... THE HTML VERSION. (cue the horror music)</p>",
+					"<br/>",
+					"<p>I really wanted this game on Newgrounds hence we needed a HTML build, basically a version of the game that can run on the web. I'm gonna try not to get too technical during this section, but if you're a programmer or a game developer, you can probably already tell what's coming.</p>",
+					"<br/>",
+					"<h2>GAMEMAKER'S HTML EXPORT</h2>",
+					"<p>GameMaker (the engine we use) has a bunch of exports, these turn your game into a playable thing, think of like an .exe file or something. There's an option for HTML, but don't be fooled. It is NOT as easy as that. For some reason, the HTML export option comes with a bunch of issues here and there that cause unexplainable things to happen in your game. I'm not gonna talk about all of them here since this post would go on forever and I don't think you want to hear a bunch of technical stuff, but here's probably the most annoying thing that happened during all this.</p>",
+					"<br/>",
+					"</div>",
+				]
+			],
+			["video", "/swacktalk/vid/swacktalk3/blog3_vid3.mp4",""],
+			[
+				"text",
+				[
+					"<div id=\"guest_onskutalk\">",
+					"<br/>",
+					"<p>WHY IS IT DOING THAT?</p>",
+					"<br/>",
+					"<p>You know that thing that happens at the end of Toxic Trenches? The big explosion before the Frankenbarf fight? Yeah, that one. Well, that almost had to be entirely removed from the Newgrounds version because of GameMaker's chicanery. \"Why?\" you might be asking, what was the issue? Well, you see, it straight up didn't happen in the HTML version. Yep, the barrel just didn't fall and cause the explosion. It works fine in the Windows version? Why not here? THE ISSUE IS MORE HORRIFYING THAN YOU MIGHT THINK.</p>",
+					"<br/>",
+					"<p>It wasn't because of a programming bug on our end, no. It was because we had set the \"depth\" value (gamemaker objects' number that tells them how they should be layered) was too big of a number, because it was supposed to be all the way in the background. That was the issue, just a number that was a bit too big. And how long did that take to figure out? A DAY OR TWO. Thanks GameMaker.</p>",
+					"<br/>",
+				]
+			],
+			["image", "/swacktalk/img/swacktalk3/blog3_img10.png",""],
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>Anyway, I'm gonna go now. I think I need to cool off. Bye.</p>",
+					"<br/>",
+				]
+			],
+			
+			[
+				"text",
+				[
+					"</div>", //close swacktalk guest div
+				]
+			],
+			
+			/* end of onsku part */
+			
+			[
+				"text",
+				[
+					"<br/>",
+					"<p>Wow Onsku that was cool.</p>",
+					"<br/>",
+					"<p>Wanna see even MORE footage of the old DynamiteHead builds? Check out this video Onsku made a LONG TIME AGO. That's the stuff, alright.</p>",
+					"<br/>",
+					"<div class=\"ytvideo\">",
+					'<iframe width="560" height="315" src="https://www.youtube.com/embed/AfoiEUsuwoU?si=0MWYYqoDihaXf94C" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+					"</div>",
+					"<br/>",
+					"<p>Yeah, that's basically it for the development process of the Toxic Demo. Some polish up here and there, a couple more tests and it sees the light of day on Newgrounds and itch.io... and Gamejolt How Could I Forget Gamejolt.</p>",
+					"<br/>",
+					"<p>Stay tuned for more swacktalk posts. I might dabble into the whole process of HOW I MAKE MY COMICS in the next post. Okay, I'm out. For real.</p>",
+				]
+			],
+		],
+	}
+];
