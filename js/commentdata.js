aa9038b040f6c54c95f6eca5355b7a24a3ad6ed7
swacky gcf <qwertysxdg3361@gmail.com>
2026-06-02 04:34:37 +0900

    Update commentdata.js

--- js/commentdata.js
+++ js/commentdata.js
@@ -1,4 +1,5 @@
 var comment_data = [
+	{ page: "/news/article/comment_section/", user: "Ax.", date: "June 1, 2026 at 07:28 PM", comment: "TF2 is BONK!", },
 	{ page: "/swacktalk/blog/developing_dynamitehead_toxic_demo/", user: "YAYSUU", date: "May 29, 2026 at 03:32 AM", comment: "and don't forget to play yaysuu's- *i am then shot 41808 times*", },
 	{ page: "/swacktalk/blog/developing_dynamitehead_toxic_demo/", user: "Eddiemancurly", date: "May 13, 2026 at 08:33 PM", comment: "What do you mean a red alcoholic? Like anton?", },
 	{ page: "/swacktalk/blog/developing_dynamitehead_toxic_demo/", user: "breadft", date: "May 13, 2026 at 07:42 PM", comment: "wow crazy talk", },
